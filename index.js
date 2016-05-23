8b4ec4ae58f51af38242b9e7de6669b5a1e48a5e
Anton Antonov <anton.synd.antonov@gmail.com>
2016-05-23 18:19:09 +0300

    Refactor and add proper fields encoding

--- index.js
+++ index.js
@@ -51,6 +51,37 @@ function decodeHeaderDate(buff) {
     return date.toUTCString();
 }
 
+function encodeHeader(buffer, fields, records) {
+    // Write version DBase 5.0
+    buffer.writeUInt8(0x05, 0);
+    const dateNow = new Date();
+    buffer.writeUInt8(dateNow.getYear(), 1);
+    buffer.writeUInt8(dateNow.getMonth(), 2);
+    buffer.writeUInt8(dateNow.getDate(), 3);
+    // TODO: actually add number of records
+    buffer.writeUInt32LE(records.length, 4);
+    // TODO: explain the header size for DBase 5.0.
+    buffer.writeUInt16LE(33 + (fields.length * 32), 8);
+    buffer.writeUInt16LE(fields.reduce((memo, field) => memo + FIELD_SIZES[field.type.charAt(0)], 1), 10);
+    return buffer;
+}
+
+function encodeField(field) {
+    let fieldBuffer = new Buffer(32);
+    fieldBuffer.asciiWrite(zeroFill(field.name, 11, 'right'), 0, 11);
+    const type = field.type.charAt(0);
+    fieldBuffer.asciiWrite(type, 11, 1);
+    // Field data address (address is set in memory; not useful on disk).
+    fieldBuffer.writeUInt32LE(0, 12, 4);
+    fieldBuffer.writeUInt8(field.length, 16, 1);
+    fieldBuffer.writeUInt8(FIELD_SIZES[type], 17);
+    fieldBuffer.writeUInt16LE(0, 18, 2); // reserved
+    fieldBuffer.writeUInt8(0, 20, 1); // work area id - reserved
+    fieldBuffer.writeUInt8(0, 21, 10); // reserved
+    fieldBuffer.writeUInt8(0, 31, 1); // index flag
+    return fieldBuffer;
+}
+
 function decodeField(buff, fieldOffset) {
     let fieldBuff = buff.slice(fieldOffset, fieldOffset + FIELD_OFFSET_SIZE);
     return {
@@ -99,64 +130,35 @@ function decodeRecords(buff, fields, header) {
     return records;
 }
 
-fs.readFile('./world.dbf', (err, buff) => {
-    // Decode
+function decode(buff) {
     const header = decodeHeader(buff);
-    console.log(header);
     const fields = decodeFields(buff);
-    let records = decodeRecords(buff, fields, header);
+    const records = decodeRecords(buff, fields, header);
+    return { header, fields, records };
+}
 
+fs.readFile('./world.dbf', (err, buff) => {
+    const { header, fields, records } = decode(buff);
     console.log('Start encoding!');
 
     // Encode back
-    let headerBuffer = new Buffer(32);
-    // Write version DBase 5.0
-    headerBuffer.writeUInt8(0x05, 0);
-    const dateNow = new Date();
-    headerBuffer.writeUInt8(dateNow.getYear(), 1);
-    headerBuffer.writeUInt8(dateNow.getMonth(), 2);
-    headerBuffer.writeUInt8(dateNow.getDate(), 3);
-    // TODO: actually add number of records
-    headerBuffer.writeUInt32LE(records.length, 4);
-    // TODO: explain the header size for DBase 5.0.
-    let headerSize = 33 + (fields.length * 32);
-    headerBuffer.writeUInt16LE(headerSize, 8);
-
-    let writeFieldOffset = FIELD_OFFSET_SIZE;
+    let headerBuffer = encodeHeader(new Buffer(32), fields, records);
 
     let fileBuffer = Buffer.concat([headerBuffer], headerBuffer.length);
 
-    let fieldBuffers = [];
+    let fieldBuffers = fields.map((field) => encodeField(field));
 
-    fields.forEach((field) => {
-        let fieldBuffer = new Buffer(32);
-        fieldBuffer.asciiWrite(zeroFill(field.name, 11, 'right'), 0, 11);
-        const type = field.type.charAt(0);
-        fieldBuffer.asciiWrite(type, 11, 1);
-        // Field data address (address is set in memory; not useful on disk).
-        fieldBuffer.writeUInt32LE(0, 12, 4);
-        fieldBuffer.writeUInt8(field.length, 16, 1);
-        fieldBuffer.writeUInt8(FIELD_SIZES[type], 17);
-        fieldBuffer.writeUInt16LE(0, 18, 2); // reserved
-        fieldBuffer.writeUInt8(0, 20, 1); // work area id - reserved
-        fieldBuffer.writeUInt8(0, 21, 10); // reserved
-        fieldBuffer.writeUInt8(0, 31, 1); // index flag
-
-        fieldBuffers.push(fieldBuffer);
-        writeFieldOffset += FIELD_OFFSET_SIZE;
-    });
-
-    fileBuffer = Buffer.concat([headerBuffer, ...fieldBuffers],
+    fileBuffer = Buffer.concat([fileBuffer, ...fieldBuffers],
                                fieldBuffers.reduce((memo, buff) => {
         return memo + buff.length;
-    }, fileBuffer.length + 1));
+    }, fileBuffer.length + 1)); // add + 1 for the field delimiter
 
-    fileBuffer.writeUInt8(FIELD_DELIMITER, writeFieldOffset, 1);
+    fileBuffer.writeUInt8(FIELD_DELIMITER, FIELD_OFFSET_SIZE * fields.length, 1);
 
     fs.writeFile('./new.dbf', fileBuffer, () => {
-        fs.readFile('./new.dbf', (err, buff) => {
+        fs.readFile('./new.dbf', (err, newBuff) => {
             console.log('NEW FILE!');
-            readBuffer(buff);
+            readBuffer(newBuff);
         });
     });
 });
